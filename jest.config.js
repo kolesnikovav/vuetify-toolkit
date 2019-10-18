bf5e31341ee74b557ded9949be357655423fcdeb
alexandr.kolesnikov <alex.v.kolesnikov72@gmail.com>
2019-10-18 13:34:23 +0300

    fix test

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   preset: '@vue/cli-plugin-unit-jest/presets/typescript-and-babel',
-  transformIgnorePatterns: ['/node_modules/(?!vuetify)']  
+  transformIgnorePatterns: ['/node_modules/(?!vuetify)']
 }
 
 // module.exports = {
--- tests/unit/VTreeSelect.spec.js
+++ tests/unit/VTreeSelect.spec.js
@@ -1,36 +1,50 @@
+// Libraries
 import Vue from 'vue'
+import Vuetify from 'vuetify'
+
+// Utilities
 import {
   mount,
-  shallowMount,
-  Wrapper,
-  MountOptions,
+  createLocalVue
 } from '@vue/test-utils'
 
 import VTreeSelect from '@/components/VTreeSelect/VTreeSelect'
 
-Vue.prototype.$vuetify = {
-  icons: {
-    values: {
-      subgroup: 'arrow_drop_down',
-      checkboxOn: 'check_box',
-      checkboxOff: 'check_box_outline_blank',
-    },
-  },
-}
+// console.log(vuetify)
+
+const localVue = createLocalVue()
 
 const singleRootTwoChildren = [
-  { id: 0, name: 'Root', children: [{ id: 1, name: 'Child' }, { id: 2, name: 'Child 2' }] },
+  { id: 0, name: 'Root', children: [{ id: 1, name: 'Child' }, { id: 2, name: 'Child 2' }] }
 ]
 
 const threeLevels = [
-  { id: 0, name: 'Root', children: [{ id: 1, name: 'Child', children: [{ id: 2, name: 'Grandchild' }] }, { id: 3, name: 'Child' }] },
+  { id: 0, name: 'Root', children: [{ id: 1, name: 'Child', children: [{ id: 2, name: 'Grandchild' }] }, { id: 3, name: 'Child' }] }
 ]
 
 describe('VTreeSelect.js', () => {
+  // let vuetify
+
+  // beforeEach(() => {
+  //   vuetify = new Vuetify()
+  // })
+  // console.log(vuetify)
+
+  let vuetify = new Vuetify({
+      $vuetify: {
+        lang: {
+          t: (val) => val
+        }
+      }
+  })
+
+  console.log(vuetify)
+
   it('renders all items', () => {
-    const wrapper = shallowMount(VTreeSelect, {
-      propsData: { 
-        items: singleRootTwoChildren 
+    const wrapper = mount(VTreeSelect, {
+      vuetify,
+      propsData: {
+        items: singleRootTwoChildren
       }
     })
     expect(wrapper.html()).toMatchSnapshot()
